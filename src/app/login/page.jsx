12ee083f75a0b2d0d690f38727cda97635ecdd9f
Mahardhika Bredy Dwi Gagah Santoso <bredymahardhika@gmail.com>
2024-08-12 11:36:47 +0700

    Login Page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,49 +1,75 @@
 'use client';
 
+import { useState } from 'react';
+
 export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (username === 'Yorkha' && password === 'yorkha1234') {
+            localStorage.setItem('username', username);
+            window.location.href = '/content';
+        } else {
+            setErrorMessage('Invalid username or password.');
+        }
+    };
+
     return (
-        <div className="flex md:flex-row h-screen bg-slate-400">
-        {/* Bagian Gambar */}
-        <div className="hidden md:flex bg-slate-400 md:w-1/2 lg:w-1/2 justify-center items-center">
-            <img src="/path-to-your-image.jpg" alt="Login Image" className="w-3/4" />
-        </div>
-        
-        {/* Bagian Form Login */}
-        <div className="bg-white w-full md:w-1/2 lg:w-1/2 flex justify-center items-center text-black lg:rounded-l-lg">
-            <div className="w-full max-w-md p-8">
-                <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-                <form>  
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            required
-                            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            required
-                            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="bg-blue-500 text-white py-2 px-4 rounded w-full hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    >
-                        Login
-                    </button>
-                </form>
+        <div className="flex md:flex-row h-screen bg-[#1c1c22] dark:bg-white">
+
+            {/* Image Part */}
+            <div className="hidden md:flex md:w-1/2 lg:w-1/2 justify-center items-center">
+                <img src="/code.png" alt="Login Image" className="w-3/4" />
+            </div>
+
+            {/* Form Login */}
+            <div className="bg-white dark:bg-[#1c1c22] w-full md:w-1/2 lg:w-1/2 flex justify-center items-center lg:rounded-l-lg">
+                <div className="w-full max-w-md p-8">
+                    <h1 className="text-4xl font-semibold text-center mb-6 text-[#1c1c22] dark:text-white">
+                        Login<span className="text-[#3287db]">.</span>
+                    </h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-4 font-semibold">
+                            <label className="block mb-2 text-[#1c1c22] dark:text-white">
+                                Email
+                            </label>
+                            <input
+                                type="text"
+                                id="username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                                className="w-full px-3 py-2 border ring-1 ring-[#03346E] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#3287db] text-[#1c1c22] "
+                            />
+                        </div>
+                        <div className="mb-6 font-semibold">
+                            <label className="block mb-2 text-[#1c1c22] dark:text-white">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                                className="w-full px-3 py-2 border ring-1 ring-[#03346E] rounded-lg focus:outline-none focus:ring-2 focus:ring-[#3287db] text-[#1c1c22]"
+                            />
+                        </div>
+                        {errorMessage && (
+                            <p className="text-red-500 text-sm mb-4">{errorMessage}</p>
+                        )}
+                        <button
+                            type="submit"
+                            className="py-2 px-4 w-full font-semibold rounded-lg bg-[#3287db] text-white hover:bg-[#03346E] focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        >
+                            Login
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
     );
 }
